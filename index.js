c114a841a2034c64bf7aa76bee6d43d0d693cfc8
Thomas <tslemoullec@connect.ust.hk>
2019-08-08 14:26:44 +0200

    Sessions / cookies ok; Add To Do ok ; Logout Ok; Home ok; Remove KO

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser');
+var session = require('cookie-session');
+var cookieParser = require('cookie-parser');
+
+app.use(bodyParser.json());
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.use(cookieParser('notsosecretkey'));
+app.use(session({secret: 'notsosecretkey123'}));
+app.use(express.static(__dirname + '/public')); 
 
 app.get('/', (req, res) => {
-  res.send('To Do List App Homepage!')
+    if (!req.session.actionsToDo) {
+        res.render('home.ejs');
+    }
+    else {
+        res.render('home.ejs', {toDo: req.session.actionsToDo})
+    }
+});
+
+app.post('/submit-action', urlencodedParser, (req, res) => {
+    const actionToDo = req.body.action
+    if (!req.session.actionsToDo) {
+        req.session.actionsToDo = new Array(actionToDo);
+    }
+    else {
+        req.session.actionsToDo.push(actionToDo);
+    }
+    res.render('home.ejs', {toDo: req.session.actionsToDo})
+    res.end()
+});
+
+app.get('/logout', (req,res) => {
+    req.session = null;
+    console.log("Cookies cleaned!");
+    res.redirect('/');
 });
 
 app.listen(8000, () => {
